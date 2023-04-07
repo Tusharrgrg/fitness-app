6baee5444ca0117cf96ea2f5992b57e6ccb87dd5
Tushar agrawal <gargtushar495@gmail.com>
2023-04-07 23:12:46 +0530

    add both pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,13 +5,13 @@ import SearchExercise from '../components/SearchExercise'
 import Exercises from '../components/Exercises'
 
 const Home = () => {
-  const [exercise , setExercise] = useState([]);
+  const [exercises , setExercises] = useState([]);
   const [bodyPart , setBodyPart] = useState('all');
   return (
     <Box>
       <HeroBanner/>
-      <SearchExercise setBodyPart={setBodyPart} setExercise={setExercise} bodyPart={bodyPart}/>
-      <Exercises setBodyPart={setBodyPart} setExercise={setExercise} bodyPart={bodyPart}/>
+      <SearchExercise setBodyPart={setBodyPart} setExercises={setExercises} bodyPart={bodyPart}/>
+      <Exercises exercises={exercises} setExercises={setExercises} bodyPart={bodyPart}/>
     </Box>
   )
 }
